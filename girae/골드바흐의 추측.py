d860121e14ac34f5f89cf37b2ccbfbcc2bc879ad
jacti <jactio99@gmail.com>
2025-05-16 23:10:37 +0900

    change code

--- girae/골드바흐의 추측.py
+++ girae/골드바흐의 추측.py
@@ -1,6 +1,6 @@
 # 테스트 위한 함수 실제 실행시 주석처리 할 것
-import sys
-sys.stdin = open("input.txt", "r")
+# import sys
+# sys.stdin = open("input.txt", "r")
 
 # n 이하의 소수 리스트를 만드는 함수
 def setPrimeList(n:int):
@@ -30,44 +30,44 @@ def setPrimeList_while(n:int):
     return primeList
 
 
-# print(setPrimeList_while(20))
-# print(setPrimeList_for(20))
+print(setPrimeList_while(20))
+print(setPrimeList_for(20))
 
-#테스트 케이스 입력
-T = int(input())
+# #테스트 케이스 입력
+# T = int(input())
 
-inputList = []
+# inputList = []
 
-for _ in range(T):
-    inputList.append(int(input()))
+# for _ in range(T):
+#     inputList.append(int(input()))
 
-#입력 중 가장 큰 값을 n으로 저장
-n = max(inputList)
-# 같이 쓸 전역 primeList 계산
-primeList = setPrimeList(n)
+# #입력 중 가장 큰 값을 n으로 저장
+# n = max(inputList)
+# # 같이 쓸 전역 primeList 계산
+# primeList = setPrimeList(n)
 
-for num in inputList:
-    # //TRY 1 : 시간초과
-    # a, b, i = 0, num, 0
-    # # a, b는 쌍을 이루므로 새 소수를 b 이전까지만 조회하면 됨
-    # while primeList[i] < b:
-    #     if num - primeList[i] in primeList:
-    #         a, b = primeList[i], num - primeList[i]
-    #     i += 1
-    # print(f"{a} {b}")
+# for num in inputList:
+#     # //TRY 1 : 시간초과
+#     # a, b, i = 0, num, 0
+#     # # a, b는 쌍을 이루므로 새 소수를 b 이전까지만 조회하면 됨
+#     # while primeList[i] < b:
+#     #     if num - primeList[i] in primeList:
+#     #         a, b = primeList[i], num - primeList[i]
+#     #     i += 1
+#     # print(f"{a} {b}")
 
-    # # //TRY 2 : pass
-    # # num/2 부터 조회 양옆으로 조회
-    half = num//2
+#     # # //TRY 2 : pass
+#     # # num/2 부터 조회 양옆으로 조회
+#     half = num//2
 
-    index = 0
-    for i, prime in enumerate(primeList):
-        if prime >= half:
-            index = i
-            break
+#     index = 0
+#     for i, prime in enumerate(primeList): 
+#         if prime >= half:
+#             index = i
+#             break
     
-    for prime in primeList[index:]:
-        if num - prime in primeList:
-            print(f"{num - prime} {prime}")
-            break
+#     for prime in primeList[index:]:
+#         if num - prime in primeList:
+#             print(f"{num - prime} {prime}")
+#             break
         
--- 안태주/금요일/골드바흐_미해결.py
+++ 안태주/금요일/골드바흐_미해결.py
@@ -1,8 +1,9 @@
 import sys
 import math
+sys.stdin = open("input.txt", "r")
 
 def is_prime(n):
-    for j in range(2, math.sqrt(n) + 1):
+    for j in range(2, int(math.sqrt(n)) + 1):
         if n % j == 0:
             return False
     return True
@@ -10,17 +11,18 @@ def is_prime(n):
 test = int(sys.stdin.readline())
 
 array = []
-for i in range(0, test):
+for _ in range(test):
     i = int(sys.stdin.readline())
     array.append(i)
 
-array.sort()
 
-for i in range(array):
-    a,b = array//2, array//2
-    if is_prime(a) and is_prime(b):
-        print("{0} {1}".format(a, b))
-    else:
-        a -=1
-        b +=1
+for inputNum in array:
+    a,b = inputNum//2, inputNum//2
+    while(b<inputNum):
+        if is_prime(a) and is_prime(b):
+            print("{0} {1}".format(a, b))
+            break
+        else:
+            a -=1
+            b +=1
          
